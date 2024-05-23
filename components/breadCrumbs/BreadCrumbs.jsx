19bd7334597027699faa035eed627de9be01e6bf
Zafarzhon <shakirowww@inbox.ru>
2024-05-23 14:26:02 +0600

    currected styles in BreadCrumbs page

--- components/breadCrumbs/BreadCrumbs.jsx
+++ components/breadCrumbs/BreadCrumbs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function BreadCrumbs() {
   return (
-    <div className=" md:h-28 relative border-b border-[#d9d9d9]">
+    <div className=" relative border-b border-[#d9d9d9]">
       <div className="inline-block absolute bottom-0">BreadCrumbs</div>
     </div>
   );
