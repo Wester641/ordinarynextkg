17f68fb239cc9f828a344dc4fdff6c2f1ac7fb0b
zafar <shakirowww@inbox.ru>
2024-06-17 14:30:18 +0600

    fix: add carousel product page and currected carousel

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -3,10 +3,11 @@
 import BreadCrumbs from "@/components/featers/breadCrumbs/BreadCrumbs";
 import { productsForBank } from "../../../lib/constans/productsForBank";
 import { useParams } from "next/navigation";
-import { Link } from "lucide-react";
 import Image from "next/image";
 import Button from "@/components/buttons/ButtonCustom";
 import ProductIdInfo from "@/components/productsCards/productIdInfo/ProductIdInfo";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function ProductDetails() {
   const params = useParams();
@@ -22,17 +23,28 @@ export default function ProductDetails() {
 
   return (
     <section className=" min-h-screen">
-      <BreadCrumbs />
+      {/* <BreadCrumbs /> */}
       <div className="flex flex-wrap xs:flex-nowrap xs:h-1/2 gap-8 m-5 ">
         <div className=" w-full xs:w-2/5 ">
           <div className="border border-[#d9d9d9] ">
-            <div className="xs:w-full h-[350px] flex items-center justify-center">
-              <Image
-                className="h-[300px] object-contain"
-                src={product.images[0]}
-                height={300}
-                alt="img-undefined"
-              />
+            <div className="xs:w-full h-[350px] flex items-center justify-center ">
+              <Carousel
+                showThumbs={false}
+                infiniteLoop
+                autoPlay
+                interval={5000}
+                className="w-full overflow-hidden h-[300px] object-contain"
+              >
+                {product.images.map((image, index) => (
+                  <Image
+                    key={index}
+                    src={image}
+                    alt={`Slide ${index}`}
+                    height={300}
+                    className="h-[300px] object-contain"
+                  />
+                ))}
+              </Carousel>
             </div>
           </div>
         </div>
--- components/simpleslider/SimpleSlider.jsx
+++ components/simpleslider/SimpleSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import imageCarousel1 from "../../public/Img/ForCarousel/ForCarousel1.png";
 import imageCarousel2 from "../../public/Img/ForCarousel/ForCarousel2.png";
@@ -12,6 +12,8 @@ import imageCarousel9 from "../../public/Img/ForCarousel/banner.jpg";
 import imageCarousel10 from "../../public/Img/ForCarousel/emotions.jpg";
 import imageCarousel11 from "../../public/Img/ForCarousel/1812.jpg";
 import Image from "next/image";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const carouselArray = [
   imageCarousel8,
@@ -24,60 +26,27 @@ const carouselArray = [
   imageCarousel6,
 ];
 
-const Slider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === carouselArray.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? carouselArray.length - 1 : prevIndex - 1
-    );
-  };
-
-  useEffect(() => {
-    const interval = setInterval(nextSlide, 11000);
-    return () => clearInterval(interval);
-  }, []);
-
+function SimpleSlider() {
   return (
-    <section className="w-[97%] m-auto">
-      <div
-        className="relative h-28 ss:h-56 w-full shadow-md
- md:h-96 overflow-hidden "
-      >
-        <div
-          className="flex w-full h-full transition-transform duration-500 ease-in-out"
-          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-        >
-          {carouselArray.map((image, index) => (
-            <div key={index} className="w-full h-full flex-shrink-0">
-              <Image
-                src={image}
-                alt={`Slide ${index}`}
-                className="w-full h-full md:object-cover"
-              />
-            </div>
-          ))}
+    <Carousel
+      className="h-96"
+      showThumbs={false}
+      infiniteLoop
+      autoPlay
+      interval={10000}
+    >
+      {carouselArray.map((image, index) => (
+        <div className="border h-96" key={index}>
+          <Image
+            className=" object-cover"
+            src={image}
+            alt={`Slide ${index}`}
+            layout="fill"
+          />
         </div>
-        <button
-          className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black sm:bg-opacity-50 bg-opacity-20 text-white sm:px-4 sm:py-2 px-2 py-1 rounded-l-md z-10"
-          onClick={prevSlide}
-        >
-          &lt;
-        </button>
-        <button
-          className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black sm:bg-opacity-50 bg-opacity-20 text-white sm:px-4 sm:py-2 px-2 py-1 rounded-r-md z-10"
-          onClick={nextSlide}
-        >
-          &gt;
-        </button>
-      </div>
-    </section>
+      ))}
+    </Carousel>
   );
-};
-export default Slider;
+}
+
+export default SimpleSlider;
