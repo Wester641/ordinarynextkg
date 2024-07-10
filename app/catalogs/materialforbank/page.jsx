153641681858bfece74a0d5d83b40ba4bdd71eff
zafar <shakirowww@inbox.ru>
2024-07-10 11:41:01 +0600

    fix: finish commit

--- app/catalogs/materialforbank/page.jsx
+++ app/catalogs/materialforbank/page.jsx
@@ -52,6 +52,45 @@ export default function MaterialForBank() {
             </div>
           ))}
         </div>
+        <div>
+          <div class="hs-dropdown relative inline-flex">
+            <button
+              id="hs-dropdown-with-dividers"
+              type="button"
+              class="hs-dropdown-toggle py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
+            >
+              Actions
+              <svg
+                class="hs-dropdown-open:rotate-180 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor" 
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="m6 9 6 6 6-6" />
+              </svg>
+            </button>
+
+            <div
+              class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 mt-2 divide-y divide-gray-200 dark:bg-neutral-800 dark:border dark:border-neutral-700 dark:divide-neutral-700"
+              aria-labelledby="hs-dropdown-with-dividers"
+            >
+              <div class="py-2 first:pt-0 last:pb-0">
+                <p
+                  class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700"
+                  href="#"
+                >
+                  Newsletter
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- components/whatsapp/WhatsAppComponent.jsx
+++ components/whatsapp/WhatsAppComponent.jsx
@@ -6,11 +6,11 @@ import Image from "next/image";
 
 export default function WhatsAppComponent() {
   return (
-    <div className="fixed z-50 right-[5%] rounded-full bottom-[10%] bg-[#000000d1] hover:bg-green ">
+    <div className="fixed z-50 right-[5%] rounded-full bottom-[10%] bg-[#000000d1] hover:bg-green spin-slow  ">
       <div className="relative ">
-        <Link className="  cursor-pointer  " href="https://wa.me/+996500370002">
+        <Link className="cursor-pointer " href="https://wa.me/+996500370002">
           <Image
-            // className="animate-ping"
+            className="animate-none"
             src={logoImage}
             width={60}
             height={60}
